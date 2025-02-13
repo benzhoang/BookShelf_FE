7c7212ec48d91cc1f236a80a646e82158fc286d5
nguyennamcn <namnqse171784@fpt.edu.vn>
2025-02-13 16:31:26 +0700

    login ex

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
 import Layout from './Layout/Layout';
+import LoginPage from './Pages/UserPage/LoginPage';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Layout Component={HomePage} />} />
+        <Route path='/' element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
